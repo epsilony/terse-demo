9d6b3cd03b7716e788cce32bbee2f79124c26a8c
Man YUAN <epsilonyuan+mb@gmail.com>
2013-01-06 22:18:51 +0800

    Build moving least square shape function and test it.

---
+++ terse_proto/tsmf/shape_func/mls.py
@@ -0,0 +1,123 @@
+'''
+Created on 2012-12-31
+
+@author: Man YUAN <epsilonyuan@gmail.com>
+'''
+import numpy as np
+from terse_proto.tsmf.util.part_diff_math import output_length_2d
+from terse_proto.tsmf.shape_func.weight_funcs import WeightFunction
+from terse_proto.tsmf.shape_func.bases_func import MonomialBases
+
+def _dists(center, coords, diff_order):
+    num_col = output_length_2d(diff_order)
+    result = np.ndarray((len(coords), num_col), dtype=np.double)
+    for i in xrange(len(coords)):
+        d_xy = center - coords[i]
+        dst = np.dot(d_xy, d_xy) ** 0.5
+        result[i][0] = dst
+        if diff_order == 1:
+            if dst == 0:
+                result[i, 1:] = 0
+            else:
+                result[i][1] = d_xy[0] / dst
+                result[i][2] = d_xy[1] / dst  
+    return result
+
+class MLS(object):
+    '''
+    classdocs
+    '''
+    def set_diff_order(self, order):
+        if order > 1:
+            raise ValueError("Only supports 0 or 1 differential order")
+        # self.bases_func.set_diff_order(order) #nonsense about mls
+        self.weight_func.set_diff_order(order)
+        self._diff_order = order
+        self.mat_As = []
+        base_dim = self.bases_func.size()
+        self._t_mat=np.ndarray((base_dim,base_dim),dtype=np.double)
+        for _i in xrange(output_length_2d(order)):
+            self.mat_As.append(np.ndarray((base_dim, base_dim), dtype=np.double))   
+    
+    def _get_diff_order(self):
+        return self._diff_order
+    
+    diff_order = property(_get_diff_order, set_diff_order)
+    
+    def __init__(self, weight_func=None, bases_func=None):
+        '''
+        Constructor
+        '''
+        if None is weight_func:
+            self.weight_func = WeightFunction()
+        else:
+            self.weight_func = weight_func
+        if None is bases_func:
+            self.bases_func = MonomialBases()
+        else:
+            self.bases_func = bases_func
+        self.set_diff_order(0)
+        
+    def _zero_mat_As(self):   
+        for mat_A in self.mat_As:
+            mat_A.fill(0)
+    
+    def _gen_mat_Bs(self,coords_lens):
+        mat_Bs = []
+        for _i in xrange(output_length_2d(self.diff_order)):
+            mat_Bs.append(np.zeros((coords_lens, self.bases_func.size())))
+        return mat_Bs
+        
+    
+    def values(self, center, coords, influence_rads, dists=None):
+        center=np.array(center,dtype=np.double)
+        
+        if None is dists:
+            dists = _dists(center, coords, self.diff_order)   
+        
+        weights = self.weight_func.values(dists, influence_rads)
+        
+        self._zero_mat_As()
+        mat_As = self.mat_As
+
+        t_mat = self._t_mat
+        
+        num_col = output_length_2d(self.diff_order)
+        
+        mat_Bs=self._gen_mat_Bs(len(coords))
+        
+        self.bases_func.set_diff_order(0)
+        for i in xrange(len(coords)):
+            nd_crd = coords[i]
+            p = self.bases_func.values(nd_crd - center)
+            
+            for diff_index in xrange(num_col):
+                w = weights[i][diff_index]
+                
+                mat_B = mat_Bs[diff_index]
+                mat_B[i] = p
+                mat_B[i] *= w
+                
+                mat_A = mat_As[diff_index]
+                
+                np.multiply(p.reshape((-1, 1)), p, t_mat)
+                t_mat *= w
+                mat_A += t_mat
+        
+        self.bases_func.set_diff_order(self.diff_order)
+        if self.diff_order > 0:
+            self.bases_func.set_diff_order(self.diff_order)
+        p = self.bases_func.values((0.0,0.0))
+        g = np.linalg.solve(mat_As[0], p[0])
+        
+        results = np.ndarray((len(coords), num_col), dtype=np.double)
+        results[:, 0] = np.dot(mat_Bs[0] , g)
+        
+        if self.diff_order > 0:
+            g_x = np.linalg.solve(mat_As[0], p[1] - np.dot(mat_As[1], g))
+            g_y = np.linalg.solve(mat_As[0], p[2] - np.dot(mat_As[2] , g))
+            results[:, 1] = np.dot(mat_Bs[1], g) + np.dot(mat_Bs[0], g_x)
+            results[:, 2] = np.dot(mat_Bs[2], g) + np.dot(mat_Bs[0], g_y)
+        
+        return results
+    
---
+++ terse_proto/tsmf/shape_func/test/mls_test.py
@@ -0,0 +1,101 @@
+'''
+Created on 2013-1-6
+
+@author: Man YUAN <epsilonyuan@gmail.com>
+'''
+from math import cos, sin, pi
+import numpy as np
+from terse_proto.tsmf.shape_func.mls import MLS
+from nose.tools import ok_
+
+def sample_interval():
+    return (1.0, 4.0)
+
+def sample_coords():
+    v = np.linspace(*sample_interval())
+    coords = [(x, y) for x in v for y in v]
+    return coords
+
+def polygon_xy(xy):
+    x, y = xy
+    val = 1.1 + 3 * x + 4.4 * y - x ** 2 - 1.3 * x * y + 2.2 * y ** 2
+    val_x = 3 - 2 * x - 1.3 * y
+    val_y = 4.4 - 1.3 * x + 4.4 * y
+    return (val, val_x, val_y)
+    
+def sin_cos_xy(xy):
+    cycle = 40
+    par = 2 * pi / cycle
+    x, y = xy
+    val = sin(x * par) * cos(y * par)
+    val_x = par * cos(x * par) * cos(y * par)
+    val_y = -par * sin(x * par) * sin(y * par)
+    return (val, val_x, val_y)
+
+def partition_of_unity_test():
+    mls = MLS()
+    mls.set_diff_order(1)
+    interval = sample_interval()
+    v = np.linspace(*interval, num=5)
+    centers = [(x, y) for x in v for y in v]
+    acts = []
+    coords = sample_coords()
+    rad = (interval[1] - interval[0]) / 50.0 * 4
+    for center in centers:
+        coords_in_rad = []
+        for coord in coords:
+            d = np.array(coord) - center
+            d = np.dot(d, d) ** 0.5
+            if d <= rad:
+                coords_in_rad.append(coord)
+        res = mls.values(center, coords_in_rad , (rad,))
+        acts.append(np.sum(res, axis=0))
+    acts = np.array(acts, dtype=np.double)
+    acts = abs(np.max(acts, axis=0))
+    ok_(acts[0] - 1 < 1e-9)
+    ok_(acts[1] < 1e-9)
+    ok_(acts[1] < 1e-9)
+
+def function_fitness_test():
+    func_errors = [(polygon_xy, 1e-10), (sin_cos_xy, 1e-5)]
+    
+    mls = MLS()
+    mls.set_diff_order(1)
+    interval = sample_interval()
+    v = np.linspace(*interval, num=4)
+    centers = [(x, y) for x in v for y in v]
+    acts = []
+    coords = sample_coords()
+    rad = (interval[1] - interval[0]) / 50.0 * 4
+    pt_vals = np.ndarray((len(coords), 3))
+    for func, err in func_errors:
+        i = 0
+        for coord in coords:
+            val = func(coord)
+            pt_vals[i] = val
+            i += 1
+        
+        
+        for center in centers:
+            coords_in_rad = []
+            indes = []
+            i = 0
+            for coord in coords:
+                d = np.array(coord) - center
+                d = np.dot(d, d) ** 0.5
+                if d <= rad:
+                    coords_in_rad.append(coord)
+                    indes.append(i)
+                i += 1
+            res = mls.values(center, coords_in_rad , (rad,))
+            
+            act_val = np.dot(res[:, 0], pt_vals[indes, 0])
+            act_val_x = np.dot(res[:, 1], pt_vals[indes, 0])
+            act_val_y = np.dot(res[:, 2], pt_vals[indes, 0])
+            acts = np.array((act_val, act_val_x, act_val_y), dtype=np.double)
+            exps = np.array(func(center), dtype=np.double)
+            max_err = np.max(np.abs(acts - exps))
+            ok_(max_err < err, str(func) + "acts:" + str(acts) + " exps:" + str(exps) + " center:" + str(center) + "max_err:"+str(max_err)+" err_lim:" + str(err))
+    
+
+
